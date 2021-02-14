ead9aa10257c22d0296d7429cf2cdb047712d886
Sethicus-Millicus <57636737+Sethicus-Millicus@users.noreply.github.com>
2021-02-14 16:35:29 -0600

    updated app
    top 5 players by  avg FD pts

--- NBAstats/tutorials/functions.py
+++ NBAstats/tutorials/functions.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import base64
 import numpy as np 
 from nba_api.stats.static import players
+from nba_api.stats.endpoints import fantasywidget
 
 
 
@@ -15,3 +16,32 @@ st.title('NBA Player Data')
 nba_players = players.get_active_players()
 
 # now we run a function to get the players info
+@st.cache
+def load_data():
+	df = fantasywidget.FantasyWidget(todays_players = 'Y')
+	df = df.get_data_frames()[0]
+	df.round(1)
+	df.rename(columns={"PLAYER_POSITION": "POS", "TEAM_ABBREVIATION": "TEAM", "FAN_DUEL_PTS":"FD"}, inplace=True)
+	df.drop(columns=['PLAYER_ID', 'TEAM_ID', 'NBA_FANTASY_PTS', 'FT_PCT'], inplace=True)
+	df = df.sort_values(by = ['TEAM'])
+	df['team_rank'] = df.groupby(['TEAM'])['FD'].rank(method='max', ascending = False).astype(int)
+	df = df[df['team_rank'] <= 5]
+	return df
+#will add a parameter for fucntion
+playerstats = load_data()
+
+#Team selection
+sorted_unique_team = sorted(playerstats.TEAM.unique())
+selected_team = st.multiselect('Team', sorted_unique_team, sorted_unique_team)
+
+
+# filtering data
+df_selected_team = playerstats[(playerstats.TEAM.isin(selected_team))]
+
+
+st.header('Display Player Stats for Today')
+st.write('Data Dimension:' + str(playerstats.shape[0]) +' rows and '+ str(playerstats.shape[0]) +' columns.')
+st.dataframe(df_selected_team)
+
+
+
